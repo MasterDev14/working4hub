ccc0296f56655fda75ed5b06b09e1e3f2f3f15c0
Real.exe <alexvalminecraft2@gmail.com>
2026-09-19 19:52:54 +0200

    Update pubhub.lua

--- pubhub.lua
+++ pubhub.lua
@@ -1,2 +1,3 @@
 loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/f116bc4d4d4a91bbe1842e99d3d7bc79.lua"))()
+task.wait(1)
 loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/0c540ea7c4d300400192c1277db68b83.lua"))()
